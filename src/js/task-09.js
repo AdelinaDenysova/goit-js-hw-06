b0025b4ce31adcb972a12a72aafefd902dc2d181
Adelina Denysova <adochkadenisovaa@gmail.com>
2022-03-20 17:24:09 +0100

    complete task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const widget = document.querySelector('.widget');
+const color = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeColor() {
+  const backgroundColor = getRandomHexColor();
+  color.textContent = backgroundColor;
+  widget.style.backgroundColor = backgroundColor;
+}
+
+button.addEventListener('click', changeColor);
